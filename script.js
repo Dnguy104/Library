bd9472aed5008df53ddee55383e725c052653910
Dnguy104 <dnguy104@ucr.edu>
2018-05-21 15:05:57 -0700

    add books to table

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = JSON.parse(localStorage.getItem("library")) || [];
+var myLibrary = JSON.parse(localStorage.getItem("library")) || [];
 
 function Book(title, author, page, read) {
 	this.title = title;
@@ -10,33 +10,48 @@ function Book(title, author, page, read) {
 function addBookToLibrary() {
 	myLibrary.push(new Book("sdfds", "dfgdf", "45", true));
 	
+	localStorage.setItem('library', JSON.stringify(myLibrary));
 	render();
 }
 
 function render() {
-	debugger;
-	if(myLibrary.length) {
-		document.querySelector('table').style.display = 'block';
-		console.log(document.querySelector('.noBook'));
-		document.querySelector('.noBook').syle.display = 'none';
-	}
-	else {
-		document.querySelector('.noBook').syle.display = 'block';
+	if(myLibrary.length === 0) {
+		document.querySelector('.noBook').style.display = 'block';
 		document.querySelector('table').style.display = 'none';
 	}
+	else {
+		document.querySelector('table').style.display = 'block';
+		document.querySelector('.noBook').style.display = 'none';
+	}
 	
-	document.querySelector('.tableData').innerHTML = myLibrary.map((book, i) => {
+	document.querySelector('tbody').innerHTML = myLibrary.map((book, i) => {
 		return `<tr>
-					<td><cite>$(book.title)</cite></td>
-					<td>$(book.author)</td>
-					<td>$(book.page)</td>
+					<td><cite>${book.title}</cite></td>
+					<td>${book.author}</td>
+					<td>${book.page}</td>
 					<td>
-						<input type="checkbox" id="$(i)">
+						<input type="checkbox" ${book.read ? 'checked' : ''}">
 					</td>
 					<td>
-						<span class="fas fas-time"></span>
+						<button type="button" class="removeButton">
+							<span class="fas fa-times fa-lg"></span>
+						</button>
 					</td>
-				</tr>
-				`
+				</tr>`;
 	}).sort().join('');
+	
+	function deleteButton(event) {
+		console.log(event);
+		
+		myLibrary = myLibrary.splice(position, 1);
+		render();
+		localStorage.setItem('library', JSON.stringify(myLibrary));
+	}
+		
+	document.querySelectorAll('.removeButton').forEach((book) => {
+		book.addEventListener('click', event => {
+			deleteButton(event);
+		});
+	});
 }
+
